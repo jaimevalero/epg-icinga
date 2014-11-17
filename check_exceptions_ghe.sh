7f91e55d217a0f81cb57d15431e333794aa34575
Jaime Valero <jaimevalero78@yahoo.es>
2014-11-17 15:00:11 +0100

    fix for v11.10.348 log changes,
    If a JSON it is included in the exception.log, then get only some fields.

--- check_exceptions_ghe.sh
+++ check_exceptions_ghe.sh
@@ -23,6 +23,7 @@ LAST_MINUTES_TO_CHECK=5
 
 # Variables
 RETURN_CODE=''
+CALCULATED_ERROR_MSG=''
 WARN_MSG=''
 ERROR_MSG=''
 
@@ -35,6 +36,7 @@ STRING_OK='OK'
 RETURN_WARN=1
 STRING_WARN='Warning'
 
+EXCEPTIONS_FILE=/var/log/github/exceptions.log
 FILE_DATE=/tmp/file_date
 FILE_TMP_DATE=/tmp/dates.check.tmp
 # End of parameters
@@ -43,61 +45,72 @@ FILE_TMP_DATE=/tmp/dates.check.tmp
 CheckLogFilesAreRotating( )
 {
 # If file size are zero due to daily rotate, we return ok, to avoid false positives
-[ ! -s /var/log/github/exceptions.log ] && echo $STRING_OK. Empty exceptions file && exit $RETURN_OK
+[ ! -s $EXCEPTIONS_FILE ] && echo $STRING_OK. Empty exceptions file && exit $RETURN_OK 
 
 }
 
 PreWork( )
 {
 
-        CheckLogFilesAreRotating
+	CheckLogFilesAreRotating
 
-        #Extract time file to parse
-        rm -f $FILE_DATE $FILE_TMP_DATE 2>/dev/null
-        for MINUTO in `seq 1 $LAST_MINUTES_TO_CHECK`
-        do
-                                echo "date -d '$MINUTO minute ago' '+%Y-%m-%d %H:%M' >> $FILE_DATE" >>  $FILE_TMP_DATE
-        done
-        chmod +x  $FILE_TMP_DATE && . $FILE_TMP_DATE
+	#Extract time file to parse
+	rm -f $FILE_DATE $FILE_TMP_DATE 2>/dev/null
+	for MINUTO in `seq 1 $LAST_MINUTES_TO_CHECK`
+	do
+				echo "date -d '$MINUTO minute ago' '+%Y-%m-%d %H:%M' >> $FILE_DATE" >>  $FILE_TMP_DATE
+	done
+	chmod +x  $FILE_TMP_DATE && . $FILE_TMP_DATE
 
-
+				
 }
-
 
 PostWork( )
 {
-        rm -f  $FILE_TMP_DATE $FILE_DATE  2>/dev/null
-
+#	rm -f  $FILE_TMP_DATE $FILE_DATE  2>/dev/null
+ a=0
 }
 
 LookForExceptions( )
 {
-     ERROR_MSG=`cat /var/log/github/exceptions.log /var/log/github/exceptions.log.1 | grep -i -f $FILE_DATE | tail -1 | grep -o -e  "Errno::[a-zA-Z0-9]*\"\,\"message\":\"[a-zA-Z0-9 \-]*" | cut -d\" -f5 `
+     ERROR_MSG=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | grep -o -e  "Errno::[a-zA-Z0-9]*\"\,\"message\":\"[a-zA-Z0-9 \-]*" | cut -d\" -f5 `
 
-     WARN_MSG=`cat /var/log/github/exceptions.log /var/log/github/exceptions.log.1 | grep -i -f $FILE_DATE  `
+     WARN_MSG=`cat  $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1   | grep -i -f $FILE_DATE  `
 }
 
 CalculateReturnCode( )
 {
-        # Calculate Error MSG
-        # 0 OK,
-        # 1 Error
-        # If we have found an errror
-        if [ `echo $ERROR_MSG | grep -i [a-z] | wc -l` -eq 1 ]
-        then
-                RETURN_CODE=$RETURN_ERROR
-                RETURN_STRING=$STRING_ERROR
-        else
-                # We return warning if any content found on the last lines of the exceptions file, no matter its contents.
+	# Calculate Error MSG
+	# 0 OK, 
+	# 1 Error
+	# If we have found an errror
+	if [ `echo $ERROR_MSG | grep -i [a-z] | wc -l` -eq 1 ] 
+	then
+		RETURN_CODE=$RETURN_ERROR
+                RETURN_STRING=$CALCULATED_ERROR_MSG
+	else
+                # We return warning if any content found on the last lines of the exceptions file, no matter its contents. 
                 if [  `echo $WARN_MSG | grep -i [a-z] | wc -l` -ne 0 ]
-                then
+		then
                         RETURN_CODE=$RETURN_WARN
-                        RETURN_STRING=$WARN_MSG
+                        RETURN_STRING=$CALCULATED_ERROR_MSG
                 else
-                        RETURN_CODE=$RETURN_OK
-                        RETURN_STRING=$STRING_OK
-                fi
-        fi
+			RETURN_CODE=$RETURN_OK
+			RETURN_STRING=$STRING_OK
+                fi 
+	fi
+
+}
+
+CalculateReturnString( )
+{
+
+MY_URL=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | sed -e 's/^.*\"url\"//g'  | cut -d \" -f2`
+MY_APP=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | sed -e 's/^.*\"app\"//g'  | cut -d \" -f2`
+MY_USER=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | sed -e 's/^.*\"user\"//g'  | cut -d \" -f2`
+MY_MESSAGE=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | sed -e 's/^.*\"message\"//g'  | cut -d \" -f2 |  cut -d'[' -f1`
+#MY_MESSAGE=`cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep -i -f $FILE_DATE | tail -1 | sed -e 's/^.*\"message\"//g'  | cut -d \" -f2`    cat $EXCEPTIONS_FILE $EXCEPTIONS_FILE.1  | grep   t0_r0 | sed -e 's/^.*\"message\"//g' |  cut -d \" -f2| cut -d'[' -f1
+CALCULATED_ERROR_MSG="App:$MY_APP user:$MY_USER url:$MY_URL message:$MY_MESSAGE"
 
 }
 
@@ -110,13 +123,12 @@ PreWork
 LookForExceptions
 
 # Calculate return code acccording results
+CalculateReturnString
 CalculateReturnCode
 
 # DeleteTempFiles
 PostWork
 
-echo $RETURN_STRING $ERROR_MSG
+echo $RETURN_STRING 
 exit $RETURN_CODE
-
-
 
